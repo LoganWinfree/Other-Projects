fab9a691a46d49a013bb8db61c15d712bc9c7d8f
Logan <loganwinfree4@gmail.com>
2025-05-30 16:49:07 -0400

    fixed write method for basic overloading

--- Image/Bitmap.py
+++ Image/Bitmap.py
@@ -3,9 +3,14 @@ class handler():
     ff = ".bmp"
 
     def __init__(self, name):
-        filename = name + self.ff
-        header = 'BM'
-        infoheader = ""
+        self.filename = name + self.ff
+        self.header = 'BM'
+        self.infoheader = ""
 
-    def write(self):
-        f = open(self.filename, "w")
+
+    def write(self, path=None):
+
+        if path == None:
+            f = open(self.filename, "w")
+        else:
+            f = open(path + self.filename, "w")
